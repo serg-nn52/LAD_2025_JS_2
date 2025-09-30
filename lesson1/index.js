d31440d0d18a4afd7047ff8350e69b848a0e5cb7
Sergey Lushkin <serg.nn1985@gmail.com>
2025-09-30 21:58:10 +0300

    Добавил lesson2

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,16 +1,15 @@
 // console.log("Hi");
 
-
 //1. let
-let userName = 'Sergey';
+let userName = "Sergey";
 // let userName;
 //some code
-userName = 'Ivan';
+userName = "Ivan";
 
 userName = 7;
 
 console.log(userName);
- 
+
 //2. const
 
 const isAdmin = true;
@@ -22,7 +21,7 @@ var age = 25;
 console.log(age);
 
 //4 - не нужно
-a = 10
+a = 10;
 
 console.log(a);
 
@@ -33,13 +32,13 @@ const userAge = 20;
 let peopleCount = 10;
 const pi = 3.14;
 
-console.log(peopleCount / 0) //Infinity
-console.log(-peopleCount / 0)  //-Infinity
-console.log('test' / 10) //NaN
+console.log(peopleCount / 0); //Infinity
+console.log(-peopleCount / 0); //-Infinity
+console.log("test" / 10); //NaN
 
 //2. string
 
-let adminName = 'Ivan'; //одинарные кавычки
+let adminName = "Ivan"; //одинарные кавычки
 adminName = "Sergey"; //двойные кавычки
 adminName = `Ilya ${userAge}`; //обратные кавычки
 
@@ -64,57 +63,59 @@ console.log(car);
 
 // 6. symbol
 
-const id = Symbol('id');
-const id2 = Symbol('id');
+const id = Symbol("id");
+const id2 = Symbol("id");
 
 console.log(id);
 
 // 7. bigint /
 
-const bigData = 1152921504606846976n;//1152921504606846976
+const bigData = 1152921504606846976n; //1152921504606846976
 
 console.log(bigData);
 
 // 8. object
 
 const user = {
-    name: 'Sergey', 
+    name: "Sergey",
     age: 30,
 };
 
 user.age = 31;
 
-user.car = 'BMW';
+user.car = "BMW";
 
 //обращение через точку
 console.log(user.name);
 
 //обращение через динамический ключ
-const key = 'age';
+const key = "age";
 console.log(user[key]);
-
 
 //массив
 
-const articles = ['О кошках', 'О природе', 'О собаках'];
+const articles = [
+    "О кошках",
+    "О природе",
+    "О собаках",
+    "О собаках",
+];
 
 console.log(articles[0]);
 
 // console.log(typeof(articles));
-console.log(typeof articles)
+console.log(typeof articles);
 
-articles.push('О людях');
+articles.push("О людях");
 
-articles[100] = 'Супер последняя статья'; //нехорошо
+articles[100] = "Супер последняя статья"; //нехорошо
 
-articles.car = 'Audi'; //нехорошо
+articles.car = "Audi"; //нехорошо
 
 console.log(typeof id);
 console.log(typeof null); //object, особенность
 console.log(typeof console.log); //function - но это объект
 
-
-
-console.log(typeof(null + 'test'), null + 'test');
-console.log(typeof null + 'test', null + 'test');
+console.log(typeof (null + "test"), null + "test");
+console.log(typeof null + "test", null + "test");
 //
--- lesson2/index.js
+++ lesson2/index.js
@@ -1,1 +1,12 @@
 //lesson2
+// const тестовая = 52; //можно, но плохая практика
+// console.log(тестовая);
+
+// console.log(typeof "true");
+
+const users = {
+    "user-1C": "Ivanov",
+    "user-oracle": "Petrov",
+};
+
+console.log(users["user-1C"]);
